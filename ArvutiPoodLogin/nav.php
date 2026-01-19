e10848018c01c3345b3b1c698430dd51e27e500c
nast <eha20082@gmail.com>
2026-01-19 13:16:01 +0200

    Update navigation for user session handling

--- ArvutiPoodLogin/nav.php
+++ ArvutiPoodLogin/nav.php
@@ -1,9 +1,11 @@
-
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
     <meta charset="utf-8">
-    <title>PHP Project 01</title>
+    <title>Arvutipood</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -15,17 +17,19 @@
             <li><a href="tervislik_lehed.php">Meist</a></li>
             <li><a href="tooted.php">Tooted</a></li>
             <li><a href="tooted(1).php">Galerii</a></li>
-            <?php
 
-            if (isset($_SESSION["useruid"]) && $_SESSION["useruid"] === "admin") {
-                echo "<li><a href='index.php'>Adminlehel</a></li>";
-            }
+            <?php
             if (isset($_SESSION["useruid"])) {
-                echo "<li><a href='logout.php'>Logout</a></li>";
-            }
-            else {
-                echo "<li><a href='signup.php'>Sign up</a></li>";
-                echo "<li><a href='login.php'>Log in</a></li>";
+                if ($_SESSION["useruid"] == "admin") {
+                    echo "<li><a href='index.php'>Admin-leht</a></li>";
+                }
+                echo "<li><a href='logout.php'>Väljalogimine</a></li>";
+
+                echo "<li class='current-user'>Tere, " . htmlspecialchars($_SESSION["useruid"]) . "!</li>";
+
+            } else {
+                echo "<li><a href='signup.php'>Registreerimine</a></li>";
+                echo "<li><a href='login.php'>Logi sisse</a></li>";
             }
             ?>
         </ul>
